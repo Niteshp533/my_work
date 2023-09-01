7488d22a291dd4557d5730a9ae176c0df62645ab
Nitesh Pal <118985025+Niteshp533@users.noreply.github.com>
2023-09-01 07:11:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            //Console.WriteLine("Happy Holi!");
+            Console.Write("Enter Your Name : ");
+            n = Console.ReadLine();
+            //Console.WriteLine("Hi, My name is "+ n);
+            Console.WriteLine("Hi, my name is {0}.", n);
+            Console.Read();
+
+        }
+    }
+}
