12dd901073bc95e245ce112b14df230d7dd94c8a
Nitesh Pal <118985025+Niteshp533@users.noreply.github.com>
2023-09-01 07:14:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace first1
+namespace p2
 {
     class Program
-    {
+    {   
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(" The sum of two given integers is {0}", c);
+        }
         static void Main(string[] args)
         {
-            string n;
-            //Console.WriteLine("Happy Holi!");
-            Console.Write("Enter Your Name : ");
-            n = Console.ReadLine();
-            //Console.WriteLine("Hi, My name is "+ n);
-            Console.WriteLine("Hi, my name is {0}.", n);
-            Console.Read();
+            int a;
+            int b;
+            Console.WriteLine("Enter Two integer : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            obj.Add(a, b);
+            Console.ReadKey();
 
         }
+    
     }
 }
